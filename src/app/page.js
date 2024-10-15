b9bda748a60a2cd34b3557fc39cc257d90152b5c
TiewTNT <tnttiew@gmail.com>
2024-10-15 10:49:07 +0700

    remove trash comments

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,23 @@
 "use client"
 
-// import { createClient } from '@/utils/supabase/server'
 import { createClient } from '@/utils/supabase/client'
-// import { cookies } from 'next/headers'
+
 import Link from 'next/link'
 import React from 'react'
 import { useState, useEffect } from 'react'
 
-// put async here
+
 export default function Home() {
-    // const cookieStore = cookies()
-    // const supabase = createClient(cookieStore)
     const supabase = createClient()
     const [inserts, setInserts] = useState()
     
     const handleInserts = (payload) => {
-        // alert('yey'+String(payload))
-        // location.reload();
         fetchData()
     }
     supabase.channel('texts').on('postgres_changes', {event: 'INSERT', schema: 'public', table: 'texts'}, handleInserts).subscribe()
     async function fetchData() {
         const { data : inserts2 } = await supabase.from('texts').select()
         setInserts(inserts2)
-        // alert(String(supabase.from('texts').select()))
     }
     useEffect(() => {
         fetchData()
